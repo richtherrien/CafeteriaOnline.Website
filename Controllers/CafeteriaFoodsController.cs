12adff9a15c519fb6c5406222d531565f6d25821
Richard Therrien <richardgtherrien@gmail.com>
2020-03-24 21:50:30 -0400

    dev-29: Added tailwindcss, added controllers and views for Meal, Company, Config, Orders

---
+++ Controllers/CafeteriaFoodsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CafeteriaOnline.Website.Data;
+using CafeteriaOnline.Website.Models;
+
+namespace CafeteriaOnline.Website.Controllers
+{
+    public class CafeteriaFoodsController : Controller
+    {
+        private readonly CafeteriaContext _context;
+
+        public CafeteriaFoodsController(CafeteriaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CafeteriaFoods
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.CafeteriaFoods.ToListAsync());
+        }
+
+        // GET: CafeteriaFoods/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cafeteriaFood = await _context.CafeteriaFoods
+                .FirstOrDefaultAsync(m => m.CafeteriaFoodId == id);
+            if (cafeteriaFood == null)
+            {
+                return NotFound();
+            }
+
+            return View(cafeteriaFood);
+        }
+
+        // GET: CafeteriaFoods/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CafeteriaFoods/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CafeteriaFoodId,Name,MealType,Price")] CafeteriaFood cafeteriaFood)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cafeteriaFood);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cafeteriaFood);
+        }
+
+        // GET: CafeteriaFoods/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cafeteriaFood = await _context.CafeteriaFoods.FindAsync(id);
+            if (cafeteriaFood == null)
+            {
+                return NotFound();
+            }
+            return View(cafeteriaFood);
+        }
+
+        // POST: CafeteriaFoods/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CafeteriaFoodId,Name,MealType,Price")] CafeteriaFood cafeteriaFood)
+        {
+            if (id != cafeteriaFood.CafeteriaFoodId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cafeteriaFood);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CafeteriaFoodExists(cafeteriaFood.CafeteriaFoodId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cafeteriaFood);
+        }
+
+        // GET: CafeteriaFoods/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cafeteriaFood = await _context.CafeteriaFoods
+                .FirstOrDefaultAsync(m => m.CafeteriaFoodId == id);
+            if (cafeteriaFood == null)
+            {
+                return NotFound();
+            }
+
+            return View(cafeteriaFood);
+        }
+
+        // POST: CafeteriaFoods/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cafeteriaFood = await _context.CafeteriaFoods.FindAsync(id);
+            _context.CafeteriaFoods.Remove(cafeteriaFood);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CafeteriaFoodExists(int id)
+        {
+            return _context.CafeteriaFoods.Any(e => e.CafeteriaFoodId == id);
+        }
+    }
+}
