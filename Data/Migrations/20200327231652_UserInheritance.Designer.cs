39ee629e75d515dc49b9bcd84b985a6bcc5edf9d
Richard Therrien <richardgtherrien@gmail.com>
2020-03-27 20:02:28 -0400

    dev-33: fixed issue when navigating to the login and register pages, fixed issue with accessing the roles table by creating new migrations

--- Data/Migrations/20200327231652_UserInheritance.Designer.cs
+++ Data/Migrations/20200327231652_UserInheritance.Designer.cs
@@ -4,14 +4,16 @@ using CafeteriaOnline.Website.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CafeteriaOnline.Website.Data.Migrations
 {
     [DbContext(typeof(CafeteriaContext))]
-    partial class CafeteriaContextModelSnapshot : ModelSnapshot
+    [Migration("20200327231652_UserInheritance")]
+    partial class UserInheritance
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
