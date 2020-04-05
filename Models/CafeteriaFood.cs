62034c98e24bd05d8134ccf9b74f1200e37739a3
Richard Therrien <richardgtherrien@gmail.com>
2020-04-05 18:43:09 -0400

    dev-10: Removed unused usings from models

--- Models/CafeteriaFood.cs
+++ Models/CafeteriaFood.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CafeteriaOnline.Website.Models
+﻿namespace CafeteriaOnline.Website.Models
 {
     public class CafeteriaFood
     {
--- Models/Users/Organizer.cs
+++ Models/Users/Organizer.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CafeteriaOnline.Website.Models
+﻿namespace CafeteriaOnline.Website.Models
 {
     public class Organizer : Employee
     {
