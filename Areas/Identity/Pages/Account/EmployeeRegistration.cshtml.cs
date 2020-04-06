0e41108feda1d96fb185c27c67b180a6e0b96bc4
Roman Makuch <5312476-rmakuch@users.noreply.gitlab.com>
2020-04-06 00:35:38 +0000

    Resolve "Page - Employee Registration MVC"

---
+++ Areas/Identity/Pages/Account/EmployeeRegistration.cshtml.cs
@@ -0,0 +1,146 @@
+using CafeteriaOnline.Website.Data;
+using CafeteriaOnline.Website.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CafeteriaOnline.Website.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class EmployeeRegistrationModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<EmployeeRegistrationModel> _logger;
+        private readonly CafeteriaContext _context;
+
+        public EmployeeRegistrationModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<EmployeeRegistrationModel> logger,
+            CafeteriaContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _context = context;
+        }
+
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            public string Name
+            {
+                get
+                {
+                    string Name = FirstName;
+                    if (!string.IsNullOrEmpty(LastName))
+                    {
+                        FirstName = " " + LastName;
+                    }
+                    return Name;
+                }
+            }
+
+            [Required]
+            [Display(Name = "Telephone")]
+            public int Telephone { get; set; }
+
+            [Required]
+            [Display(Name = "Employee ID")]
+            public string CompanyID { get; set; }
+
+            [Required]
+            [Display(Name = "CompanyCode")]
+            public string CompanyCode { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (ModelState.IsValid)
+            {
+                var user = new Employee { UserName = Input.Email, Email = Input.Email };
+
+                Company comp = _context.Companies.Where(p => p.CompanyCode == Input.CompanyCode).FirstOrDefault();
+
+                //omp.Employees.Add(user);
+                user.CompanyId = comp.CompanyId;
+
+
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, "Employee");
+
+
+
+
+                    _logger.LogInformation("User created a new account with password.");
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}
